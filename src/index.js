305cf2d2c932969cbe9b81b45bed7f19b5c77773
Nurbol <nurbol.sultanov@gmail.com>
2023-12-05 21:38:40 -0800

    Fix cors configurations

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import createRoot from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './components/App';
 
-createRoot.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
 );
